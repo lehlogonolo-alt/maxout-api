0e9fb15f5a954f877543cea917fefbd3f39f9191
lehlogonolo-alt <hlogisj@gmail.com>
2025-09-24 22:46:43 +0200

    Update seed.js

--- seed.js
+++ seed.js
@@ -9,14 +9,15 @@ mongoose.connect(process.env.MONGODB_URI, {
 });
 
 Workout.insertMany([
-  { title: 'Squats', details: '12 min • 120 kcal', imageUrl: 'squatt.png' },
-  { title: 'Push Ups', details: '10 min • 80 kcal', imageUrl: 'pushup.png' },
-  { title: 'Abs', details: '10 min • 90 kcal', imageUrl: 'abs.png' },
-  { title: 'Leg Day', details: '10 min • 90 kcal', imageUrl: 'legday.png' },
-  { title: 'Full Body Stretching', details: '15 min • 90 kcal', imageUrl: 'stretching.png' },
-  { title: 'Running', details: '20 min • 200 kcal', imageUrl: 'running.png' }
+      { title: 'Squats', details: '12 min • 120 kcal', imageUrl: 'squatt.jpg' },
+      { title: 'Push Ups', details: '10 min • 80 kcal', imageUrl: 'pushup.jpg' },
+      { title: 'Abs', details: '10 min • 90 kcal', imageUrl: 'abs.jpg' },
+      { title: 'Leg Day', details: '10 min • 90 kcal', imageUrl: 'legday.jpg' },
+      { title: 'Full Body Stretching', details: '15 min • 90 kcal', imageUrl: 'stretching.jpg'},
+      { title: 'Running', details: '20 min • 200 kcal', imageUrl: 'running.jpeg' }
 ]).then(() => {
   console.log('Workouts seeded');
   mongoose.disconnect();
 });
+
 
