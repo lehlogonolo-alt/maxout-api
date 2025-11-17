4c440452c5555c28f4ce0118c3cac4e868109d35
lehlogonolo-alt <hlogisj@gmail.com>
2025-11-17 02:32:36 +0200

    Modify workout details in seed.js
    Updated workout details and adjusted seeding values.

--- seed.js
+++ seed.js
@@ -9,15 +9,16 @@ mongoose.connect(process.env.MONGODB_URI, {
 });
 
 Workout.insertMany([
-      { title: 'Squats', details: '12 min • 120 kcal', imageUrl: 'squatt.jpg' },
+      { title: 'Squats', details: '10 min • 120 kcal', imageUrl: 'squatt.jpg' },
       { title: 'Push Ups', details: '10 min • 80 kcal', imageUrl: 'pushup.jpg' },
       { title: 'Abs', details: '10 min • 90 kcal', imageUrl: 'abs.jpg' },
       { title: 'Leg Day', details: '10 min • 90 kcal', imageUrl: 'legday.jpg' },
-      { title: 'Full Body Stretching', details: '15 min • 90 kcal', imageUrl: 'stretching.jpg'},
-      { title: 'Running', details: '20 min • 200 kcal', imageUrl: 'running.jpeg' }
+      { title: 'Full Body Stretching', details: '10 min • 90 kcal', imageUrl: 'stretching.jpg'},
+      { title: 'Running', details: '10 min • 200 kcal', imageUrl: 'running.jpeg' }
 ]).then(() => {
   console.log('Workouts seeded');
   mongoose.disconnect();
 });
+
 
 
