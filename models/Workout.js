6fab48b6f32b49558af83a73967bc66a27439d86
lehlogonolo-alt <hlogisj@gmail.com>
2025-11-18 23:34:56 +0200

    Update Workout.js

--- models/Workout.js
+++ models/Workout.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
-const WorkoutSchema = new mongoose.Schema({
-  title: String,
-  details: String,
-  imageUrl: String
-});
+const WorkoutSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    details: { type: String, required: true },
+    imageUrl: { type: String }
+  },
+  { timestamps: true } //  adds createdAt and updatedAt automatically
+);
 
 module.exports = mongoose.model('Workout', WorkoutSchema);
+
+
 
 
